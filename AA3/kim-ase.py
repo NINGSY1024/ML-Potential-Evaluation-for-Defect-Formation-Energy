dc91273b490640b0bf4d74a76aef239084114279
NINGSY1024 <151368813+NINGSY1024@users.noreply.github.com>
2025-03-31 12:47:46 +0800

    Add files via upload

---
+++ AA3/kim-ase.py
@@ -0,0 +1,62 @@
+from ase.io import read, write
+from ase.constraints import ExpCellFilter
+from ase.optimize import BFGS, FIRE, GPMin
+from ase.optimize.sciopt import SciPyFminBFGS
+from ase import Atoms
+
+def relax_structure(structure: Atoms,
+                    fmax: float = 0.01,
+                    steps: int = 20000,
+                    minimizer: str = 'fire',
+                    constant_cell: bool = False,
+                    constant_volume: bool = False,
+                    **kwargs) -> None:
+    """Relaxes the given structure.
+
+    Parameters
+    ----------
+    structure
+        atomic configuration to relax
+    fmax
+        if the absolute force for all atoms falls below this value the relaxation is stopped
+    steps
+        maximum number of relaxation steps the minimizer is allowed to take
+    minimizer
+        minimizer to use; possible values: 'bfgs', 'fire', 'gpmin', 'bfgs-scipy'
+    constant_cell
+        if True do not relax the cell or the volume
+    constant_volume
+        if True relax the cell shape but keep the volume constant
+    kwargs
+        keyword arguments to be handed over to the minimizer; possible arguments can be found
+        in the `ASE documentation <https://wiki.fysik.dtu.dk/ase/ase/optimize.html>`_
+    """
+    if structure.calc is None:
+        raise ValueError('Structure has no attached calculator object')
+    if constant_cell:
+        ucf = structure
+    else:
+        ucf = ExpCellFilter(structure, constant_volume=constant_volume)
+    # kwargs['logfile'] = kwargs.get('logfile', None)
+    if minimizer == 'bfgs':
+        dyn = BFGS(ucf, **kwargs)
+        dyn.run(fmax=fmax, steps=steps)
+    elif minimizer == 'bfgs-scipy':
+        dyn = SciPyFminBFGS(ucf, **kwargs)
+        dyn.run(fmax=fmax, steps=steps)
+    elif minimizer == 'fire':
+        dyn = FIRE(ucf, **kwargs)
+        dyn.run(fmax=fmax, steps=steps)
+    elif minimizer == 'gpmin':
+        dyn = GPMin(ucf, **kwargs)
+        dyn.run(fmax=fmax, steps=steps)
+    else:
+        raise ValueError(f'Unknown minimizer: {minimizer}')
+
+label = "hNN-Grx"
+from ase.calculators.kim.kim import KIM
+calc = KIM("hNN_WenTadmor_2019Grx_C__MO_421038499185_001")
+structure = read("DFT/CONTCAR")
+structure.set_calculator(calc)
+relax_structure(structure)
+write(f"model_{label}_optimize.xyz", structure)
